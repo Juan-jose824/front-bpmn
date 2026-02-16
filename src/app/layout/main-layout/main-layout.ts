38a1f480c9eb56463b6b34bfeab617d0b32bc552
MaxLopez22s <max.lopez.22s@utzmg.edu.mx>
2026-02-16 15:54:11 -0600

    perfi y modo oscuro

--- src/app/layout/main-layout/main-layout.ts
+++ src/app/layout/main-layout/main-layout.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterLink, RouterOutlet, RouterModule, Router } from "@angular/router";
 import { CommonModule } from '@angular/common'
+import { HostListener } from '@angular/core';
+import { NgZone } from '@angular/core';
 
 @Component({
   selector: 'app-main-layout',
@@ -15,14 +17,18 @@ export class MainLayout implements OnInit {
   userRole: string = '';
   isMenuOpen = false;
   isProfileOpen = false;
+  refreshKey = 0;
+  isDarkMode = false;
 
   profileImage: string |null = null;
 
   // Inyectamos el router en el constructor
-  constructor(private router: Router) {}
+  constructor(private router: Router, private zone:NgZone) {}
 
   ngOnInit() {
     const data = localStorage.getItem('usuario');
+    const saveTheme = localStorage.getItem('theme');
+
     if (data) {
       const user = JSON.parse(data);
       this.userName = user.name || user.user_name;
@@ -30,6 +36,11 @@ export class MainLayout implements OnInit {
       this.profileImage = user.profileImage || null;
     } else {
       this.router.navigate(['/login']);
+    }
+
+    if (saveTheme === 'dark') {
+      document.body.classList.add('dark-theme');
+      this.isDarkMode = true;
     }
   }
 
@@ -56,20 +67,60 @@ export class MainLayout implements OnInit {
     const file = event.target.files[0];
     if (!file) return;
 
+    //Validar tipo
+    if (!file.type.startsWith('image/')) {
+      alert('Solo se permiten imágenes.');
+      return;
+    }
+
+    //Validar tamaño (2MB)
+    const maxSize = 2 * 1024 * 1024;
+    if (file.size > maxSize) {
+      alert('La imagen no debe superar los 2MB');
+      return;
+    }
+
     const reader = new FileReader();
 
     reader.onload = () => {
-      this.profileImage = reader.result as string;
+      const imageBase64 = reader.result as string;
 
-      //Guardar en localStorage
-      const data = localStorage.getItem('usuario');
-      if (data) {
-        const user = JSON.parse(data);
-        user.profileImage = this.profileImage;
-        localStorage.setItem('usuario',JSON.stringify(user));
-      }
+      this.profileImage = null;
+
+      setTimeout(() => {
+        this.profileImage = imageBase64;
+        this.refreshKey++;
+        //Guardar en localStorage
+        const data = localStorage.getItem('usuario');
+        if (data) {
+          const user = JSON.parse(data);
+          user.profileImage = imageBase64;
+          localStorage.setItem('usuario',JSON.stringify(user));
+        }
+      });
+      
+      event.target.value = '';
     };
 
     reader.readAsDataURL(file);
+  }
+
+  @HostListener('document:keydown.escape')
+  handleEscape() {
+    if (this.isProfileOpen) {
+      this.isProfileOpen = false;
+    }
+  }
+
+  toggleTheme() {
+    this.isDarkMode = !this.isDarkMode;
+
+    if (this.isDarkMode) {
+      document.body.classList.add('dark-theme');
+      localStorage.setItem('theme', 'dark');
+    } else {
+      document.body.classList.remove('dark-theme');
+      localStorage.setItem('theme', 'light');
+    }
   }
 }
